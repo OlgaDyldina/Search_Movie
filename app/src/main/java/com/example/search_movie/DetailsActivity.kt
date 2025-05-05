6d79021b4a060be7636c90d38386c71e93028cd4
Olga Dyldina <olga.dildina@yandex.ru>
2025-05-05 16:34:50 +0500

    Complite Module 27

---
+++ app/src/main/java/com/example/search_movie/DetailsActivity.kt
@@ -0,0 +1,27 @@
+package com.example.search_movie
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+
+        setFilmsDetails()
+    }
+
+    private fun setFilmsDetails() {
+        //Получаем наш фильм из переданного бандла
+        val film = intent.extras?.get("film") as Film
+
+        //Устанавливаем заголовок
+        details_toolbar.title = film.title
+        //Устанавливаем картинку
+        details_poster.setImageResource(film.poster)
+        //Устанавливаем описание
+        details_description.text = film.description
+    }
+}{
+}
