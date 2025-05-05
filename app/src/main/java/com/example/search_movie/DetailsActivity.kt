51177b50a93a9d57abc0a794b3869fd688c47115
Olga Dyldina <olga.dildina@yandex.ru>
2025-05-05 17:18:29 +0500

    Complite Module 27

--- app/src/main/java/com/example/search_movie/DetailsActivity.kt
+++ app/src/main/java/com/example/search_movie/DetailsActivity.kt
@@ -13,14 +13,10 @@ class DetailsActivity : AppCompatActivity() {
     }
 
     private fun setFilmsDetails() {
-        //Получаем наш фильм из переданного бандла
         val film = intent.extras?.get("film") as Film
 
-        //Устанавливаем заголовок
         details_toolbar.title = film.title
-        //Устанавливаем картинку
         details_poster.setImageResource(film.poster)
-        //Устанавливаем описание
         details_description.text = film.description
     }
 }{
