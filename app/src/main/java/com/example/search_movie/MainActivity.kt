90894d3afb80ed7e5bc94545e851ac92aee687d0
Olga Dyldina <olga.dildina@yandex.ru>
2025-05-06 15:12:04 +0500

    Complite Module 27

--- app/src/main/java/com/example/search_movie/MainActivity.kt
+++ app/src/main/java/com/example/search_movie/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.search_movie
 
-import android.content.Intent
 import android.os.Bundle
-import android.support.v7.widget.LinearLayoutManager
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
