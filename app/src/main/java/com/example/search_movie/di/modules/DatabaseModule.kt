03951c7afb2ab810b765005096ed1c99f66eb75b
Olga Dyldina <olga.dildina@yandex.ru>
2025-07-14 11:17:32 +0500

    Complite Module 37

---
+++ app/src/main/java/com/example/search_movie/di/modules/DatabaseModule.kt
@@ -0,0 +1,13 @@
+package com.example.search_movie.di.modules
+
+import com.example.search_movie.data.MainRepository
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class DatabaseModule {
+    @Provides
+    @Singleton
+    fun provideRepository() = MainRepository()
+}
