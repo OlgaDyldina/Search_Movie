d9cede9d1bde4d4e6f346a56704fd3abd5063405
Olga Dyldina <olga.dildina@yandex.ru>
2025-08-07 12:52:37 +0500

    Complite Module 39

--- app/src/main/java/com/example/search_movie/viewmodel/HomeFragmentViewModel.kt
+++ app/src/main/java/com/example/search_movie/viewmodel/HomeFragmentViewModel.kt
@@ -24,6 +24,7 @@ class HomeFragmentViewModel  : ViewModel() {
             }
 
         override fun onFailure() {
+            filmsListLiveData.postValue(interactor.getFilmsFromDB())
         }
     })
 }
